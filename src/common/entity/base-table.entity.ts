3793dcb609a8b245377fa56c49e9776c06acc334
saysuhyun <saysuhyun@gmail.com>
2025-01-23 01:56:26 +0900

    Serialization

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,12 +1,17 @@
+import { ExceptionsHandler } from "@nestjs/core/exceptions/exceptions-handler";
+import { Exclude } from "class-transformer";
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from "typeorm";
 
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude() // 응답값에 안 넣어도 되는 경우
   createAt: Date;
 
   @UpdateDateColumn()
+  @Exclude()
   updateAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
